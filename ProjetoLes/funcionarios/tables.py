b7350ddaaf4bfbc9a79e245ece3d9c87da0834f1
Rafael Figueira <61553927+RafaelSFigueira@users.noreply.github.com>
2022-06-08 14:54:53 +0100

    Delete ProjetoLes directory

--- ProjetoLes/funcionarios/tables.py
+++
@@ -1,8 +0,0 @@
-import django_tables2 as tables
-from .models import *
-
-class FuncionariosTable(tables.Table):
-    class Meta:
-        model = Funcionario
-        sequence = ("id", "tipofuncionarioid", "restauranteid", "nome", "telefone", "email" )
-        exclude = ("password", )
